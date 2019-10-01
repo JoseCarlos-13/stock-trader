5fcabc94f2c4b1804b145a4384d5be9de40021bf
José Carlos <jose.carlos@safetysystemtechnology.com.br>
2019-10-01 16:43:53 -0300

    the list of stocks still in development

--- src/main.js
+++ src/main.js
@@ -2,10 +2,11 @@ import Vue from 'vue'
 import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
-
+import store from './components/store/store'
 Vue.config.productionTip = false
 
 new Vue({
+	store,
 	router,
 	render: h => h(App),
 }).$mount('#app')
